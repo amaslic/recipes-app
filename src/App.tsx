a5db3e468d9c9731f8f079460aed3c7bd0607ddf
Aleksandar Maslic <aleksandar.maslic.93@gmail.com>
2021-03-13 11:14:41 +0100

    Replaced react helmet with react helmet async and introduced interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import IRecipes from './interface/recipes';
 
-function App() {
+const App = () => {
+
+  const [recipes, setRecipes] = useState<IRecipes[]>([]);
+
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data.meals);
+
+  }
+
   return (
-    <>
-    </>
+    <HelmetProvider>
+      <div className="App">
+        <Helmet>
+          <title>Recipes app</title>
+          <meta name="description" content="Recipes app" />
+          <meta name="keywords" content="React, Typescript, Recipe, Recipes, Chicken, Pizza, Pasta"></meta>
+        </Helmet>
+        <header>
+
+        </header>
+      </div>
+    </HelmetProvider>
+
   );
 }
 
---
+++ src/interface/recipes.tsx
@@ -0,0 +1,6 @@
+export default interface IRecipes {
+	idMeal: string,
+	strMeal: string,
+	strInstructions: string,
+	strMealThumb: string
+}
