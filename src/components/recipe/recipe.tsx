4df91c89f9a1940bfa32f4256317a5971073b733
Aleksandar Maslic <aleksandar.maslic.93@gmail.com>
2021-03-14 13:35:27 +0100

    Increased width of recipes box

--- src/components/recipe/recipe.tsx
+++ src/components/recipe/recipe.tsx
@@ -6,19 +6,19 @@ const Recipe = (props: IRecipes) => {
 
 	return (
 		<>
-		<div className="recipe-box" data-testid="recipe-cmp">
+			<div className="recipe-box" data-testid="recipe-cmp">
 
-			<img src={props.strMealThumb} alt={props.strMeal} loading="lazy" />
-			<div className="recipe-info">
-				<div className="title">
-					{props.strMeal}
+				<img src={props.strMealThumb} alt={props.strMeal} loading="lazy" />
+				<div className="recipe-info">
+					<div className="title">
+						{props.strMeal}
+					</div>
 				</div>
-			</div>
-			<div className="recipe-over">
-				<span>
-					{props.strInstructions}
-				</span>
-			</div>
+				<div className="recipe-over">
+					<span>
+						{props.strInstructions}
+					</span>
+				</div>
 			</div>
 		</>
 	);
